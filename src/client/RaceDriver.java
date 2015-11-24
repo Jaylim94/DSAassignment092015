a5ef619d77ce52ba610a1f8ae6bd9ff53f7de58b
User <User@192.168.0.14>
2015-11-24 09:36:55 +0800

    Add new QUestion entity and database

--- src/client/RaceDriver.java
+++ src/client/RaceDriver.java
@@ -3,21 +3,41 @@ package client;
 
 import controller.ctrlMaintainPlayer;
 import entity.Player;
-import java.util.Random;
+import java.util.*;
 
 /**
  *
- * @author Jaylim
+ * @author Jaylim, ZF
  */
 public class RaceDriver {
     Player player = new Player();
     ctrlMaintainPlayer playerControl = new ctrlMaintainPlayer();
     
     public static void main(String[] args) { 
-        
-       for(int i=0; i<50; i++)
-        System.out.println(dice());
        
+       Scanner scan = new Scanner(System.in);
+       String selection;
+        
+        System.out.println("\n");
+    		System.out.println("1. Register and Start the game");
+	    	System.out.println("2. View Ranking");
+	    	System.out.println("3. Exit");
+	    	System.out.print("Selection: ");
+                selection = scan.next();
+                
+                if(selection.equals("1")){
+	    		registerMember();
+                        gameStart();
+	    	}else if(selection.equals("2")){
+	    		viewRanking();
+	    	}else if(selection.equals("3")){
+	    		System.exit(-1);
+	    	}else{
+	    		System.out.println("Error. Please enter again.");
+	    		System.out.println();
+                }
+                    
+       // System.out.println(dice()); to roll dice
     }
     
     public static int dice() {
@@ -33,6 +53,22 @@ public class RaceDriver {
     }
     
     
+    private static void registerMember() {
+        String name;
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("\n-Registration-\nPlayer Name:");
+        name = scan.nextLine();
+        
+    }
+
+    private static void gameStart() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void viewRanking() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
 }
